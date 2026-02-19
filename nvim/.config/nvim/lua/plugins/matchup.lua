e24863124a5bf29d40b1b8b51cc30b45c28868a6
eccgecko <36884529+eccgecko@users.noreply.github.com>
2026-02-19 15:08:46 +0000

    Add/edit initial andymass/vim-matchup plugin

--- nvim/.config/nvim/lua/plugins/matchup.lua
+++ nvim/.config/nvim/lua/plugins/matchup.lua
@@ -1,14 +1,17 @@
 return {
-  -- disable built-in matchit and matchparen since vim-matchup supersedes them
   { "matchit", enabled = false },
   { "matchparen", enabled = false },
 
-  -- add vim-matchup
   {
     "andymass/vim-matchup",
-    event = "BufReadPost",
-    config = function()
-      vim.g.matchup_matchparen_offscreen = { method = "popup" }
-    end,
+    ---@type matchup.Config
+    opts = {
+      matchparen = {
+        offscreen = { method = "popup" },
+      },
+      treesitter = {
+        stopline = 500,
+      },
+    },
   },
 }
