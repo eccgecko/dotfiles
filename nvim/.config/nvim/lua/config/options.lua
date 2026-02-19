c605d9f8623b7c30d2c5bd1fc0d98e226735d200
eccgecko <36884529+eccgecko@users.noreply.github.com>
2026-02-19 16:33:38 +0000

    Add OC52 clipboard option to fix remote clipboard copy not working over ssh

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -1,3 +1,25 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.env.SSH_TTY then
+  vim.opt.clipboard:append("unnamedplus")
+
+  local function paste()
+    return {
+      vim.split(vim.fn.getreg(""), "\n"),
+      vim.fn.getregtype(""),
+    }
+  end
+
+  vim.g.clipboard = {
+    name = "OSC 52",
+    copy = {
+      ["+"] = require("vim.ui.clipboard.osc52").copy("+"),
+      ["*"] = require("vim.ui.clipboard.osc52").copy("*"),
+    },
+    paste = {
+      ["+"] = paste,
+      ["*"] = paste,
+    },
+  }
+end
